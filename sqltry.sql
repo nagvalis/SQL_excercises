2535eb8f1ba56ed515442bf4ce0a4fed2c961f1c
nagvalis <salomeja.rain@gmail.com>
2021-05-16 12:07:07 +0200

    from 15th of may

--- sqltry.sql
+++ sqltry.sql
@@ -10,12 +10,6 @@
      FROM accounts JOIN orders ON orders.account_id = accounts.id;
 
 
--- Provide a table for all web_events associated with account name of Walmart. There should be three columns. 
--- Be sure to include the primary_poc, time of the event, and the channel for each event. Additionally, you might 
--- choose to add a fourth column to assure only Walmart events were chosen.
-
-
-
 
 -- w3 List the number of customers in each country, ordered by the country with the most customers first.
 SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country ORDER BY COUNT(CustomerID) DESC;
@@ -25,16 +19,16 @@ SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country ORDER BY COUNT
 --   the event, and the channel for each event. Additionally, you might choose
 --    to add a fourth column to assure only Walmart events were chosen.
 
-SELECT accounts.primary_poc, web_events.occurred_at, web_events.channel FROM web_events
- JOIN accounts ON web_events.account_id = accounts.id  WHERE accounts.name = 'Walmart';
-
+SELECT * FROM web_events JOIN accounts ON web_events.account_id = accounts.id
+ WHERE accounts.name = 'Walmart';
 
 
 --  Provide a table that provides the region for each sales_rep along with their 
 --  associated accounts. Your final table should include three columns: the region
 --   name, the sales rep name, and the account name. Sort the accounts alphabetically
 --    (A-Z) according to account name.
-
+SELECT sales_reps.name, accounts.name, region.name FROM sales_reps JOIN accounts
+ ON sales_reps.id = accounts.sales_rep_id JOIN region ON sales_reps.region_id = region.id ORDER BY accounts.name;
 
 
 -- Provide the name for each region for every order, as well as the account
@@ -42,6 +36,9 @@ SELECT accounts.primary_poc, web_events.occurred_at, web_events.channel FROM web
 --   Your final table should have 3 columns: region name, account name, and 
 --   unit price. A few accounts have 0 for total, so I divided by (total + 0.01) 
 --   to assure not dividing by zero.
+
+
+
 
 -- Now, select the 2 tables with a join so that you can see each person's name 
 -- next to their hobby.
@@ -81,3 +78,13 @@ INSERT INTO orders (customer_id, item, price)
 INSERT INTO orders (customer_id, item, price)
     VALUES (1, "TARDIS", 1000000.00);
 
+SELECT customers.name, customers.email, orders.item, orders.price 
+FROM customers LEFT OUTER JOIN orders ON orders.customer_id = customers.id;
+
+-- Now, create another query that will result in one row per each customer, with
+--  their name, email, and total amount of money they've spent on orders. Sort the rows
+--   according to the total money spent, from the most spent to the least spent.
+-- (Tip: You should always GROUP BY on the column that is most likely to be unique in a row.)
+SELECT customers.name, customers.email, SUM(orders.price) AS total_sum
+ FROM customers LEFT OUTER JOIN orders ON orders.customer_id = customers.id
+  GROUP BY customers.id ORDER BY orders.price DESC;
