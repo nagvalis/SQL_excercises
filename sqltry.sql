7ac9d69ead259d857a0056f1d6a68ff61fb9864c
nagvalis <73224312+nagvalis@users.noreply.github.com>
2021-05-23 19:34:07 +0200

    Update sqltry.sql
    new day new lesson

--- sqltry.sql
+++ sqltry.sql
@@ -180,3 +180,86 @@ SELECT occurred_at FROM web_events ORDER BY occurred_at DESC LIMIT 1;
 -- average amount.
 SELECT AVG(poster_amt_usd) as posterprice, AVG(poster_qty) posterqty, AVG(standard_amt_usd) standardprice,
 AVG(standard_qty) standardqty, AVG(gloss_amt_usd) glossprice, AVG(gloss_qty) glossqty FROM orders;
+
+-- Questions: GROUP BY
+
+-- 1.Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+SELECT accounts.name, orders.occurred_at FROM accounts JOIN orders ON accounts.id = orders.account_id
+GROUP BY orders.occurred_at, accounts.name LIMIT 1;
+-- 2.Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.
+SELECT accounts.name, SUM(orders.total_amt_usd) FROM accounts JOIN orders ON accounts.id = orders.account_id
+GROUP BY accounts.name;
+-- 3.Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.
+SELECT web_events.occurred_at, web_events.channel, accounts.name FROM web_events JOIN accounts ON accounts.id = web_events.account_id
+ORDER BY web_events.occurred_at DESC LIMIT 1;
+-- 4.Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.
+SELECT w.channel, COUNT(b.channel) AS count FROM web_events w JOIN web_events b ON w.id = b.id GROUP BY w.channel;
+-- 5.Who was the primary contact associated with the earliest web_event?
+SELECT accounts.primary_poc FROM accounts JOIN web_events ON accounts.id = web_events.account_id
+ORDER BY web_events.occurred_at LIMIT 1;
+-- 6.What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+SELECT accounts.name, MIN(orders.total_amt_usd) minimum FROM accounts JOIN orders ON accounts.id = orders.account_id
+GROUP BY accounts.name ORDER BY minimum;
+-- 7.Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.
+SELECT r.name, COUNT(s.name) number FROM region r JOIN sales_reps s ON r.id = s.region_id GROUP BY r.name;
+
+
+
+-- Questions: GROUP BY Part II
+
+-- 1. For each account, determine the average amount of each type of paper they purchased across their orders. Your result should have four columns - one for the account name and one for the average quantity purchased for each of the paper types for each account.
+SELECT a.name, AVG(o.standard_qty) standard, AVG(o.gloss_qty) gloss, AVG(o.poster_qty) poster
+FROM accounts a JOIN orders o ON a.id = o.account_id GROUP BY a.name;
+
+-- 2. For each account, determine the average amount spent per order on each paper type. Your result should have four columns - one for the account name and one for the average amount spent on each paper type.
+SELECT a.name, AVG(o.standard_amt_usd) standard, AVG(o.gloss_amt_usd) gloss, AVG(o.poster_amt_usd) poster
+FROM accounts a JOIN orders o ON a.id = o.account_id GROUP BY a.name;
+
+-- 3. Determine the number of times a particular channel was used in the web_events table for each sales rep. Your final table should have three columns - the name of the sales rep, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+SELECT s.name, w.channel chn, COUNT(w.occurred_at) occ FROM web_events w JOIN accounts ON w.account_id = accounts.id
+JOIN sales_reps s ON accounts.sales_rep_id = s.id GROUP BY s.name, chn ORDER BY occ DESC;
+
+-- 4. Determine the number of times a particular channel was used in the web_events table for each region. Your final table should have three columns - the region name, the channel, and the number of occurrences. Order your table with the highest number of occurrences first.
+SELECT r.name, w.channel chn, COUNT(w.occurred_at) occ FROM web_events w JOIN accounts ON w.account_id = accounts.id 
+JOIN sales_reps s ON accounts.sales_rep_id = s.id JOIN region r ON r.id = s.region_id GROUP BY chn, r.name ORDER BY occ DESC;
+
+
+
+-- Questions: DISTINCT
+-- 1. Use DISTINCT to test if there are any accounts associated with more than one region.
+SELECT DISTINCT a.name FROM accounts a JOIN sales_reps s ON a.sales_rep_id = s.id JOIN region r ON s.region_id = r.id;
+
+-- 2. Have any sales reps worked on more than one account?
+SELECT DISTINCT s.name FROM sales_reps s JOIN accounts a ON s.id = a.sales_rep_id WHERE a.id > 1;
+
+
+-- Questions: HAVING
+
+-- 1. How many of the sales reps have more than 5 accounts that they manage?
+
+
+-- 2. How many accounts have more than 20 orders?
+
+
+-- 3. Which account has the most orders?
+
+
+-- 4. Which accounts spent more than 30,000 usd total across all orders?
+
+
+-- 5. Which accounts spent less than 1,000 usd total across all orders?
+
+
+-- 6. Which account has spent the most with us?
+
+
+-- 7. Which account has spent the least with us?
+
+
+-- 8. Which accounts used facebook as a channel to contact customers more than 6 times?
+
+
+-- 9. Which account used facebook most as a channel?
+
+
+-- 10. Which channel was most frequently used by most accounts?
